2bb4eba6f1f98de32c3b01e83d8eef28661c5d31
queryops <gallilloss@gmail.com>
2026-03-26 23:18:57 -0500

    Second commit

--- .next/dev/static/chunks/app_page_tsx_14f443f0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f8664932._.js",
-  "static/chunks/node_modules_2984e998._.js"
-],
-    source: "dynamic"
-});
